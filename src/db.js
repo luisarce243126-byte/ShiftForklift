3b6e494d2d0c9203d6209d949e10ecd697e28209
luisarce243126-byte <luis.arce243126@potros.itson.edu.mx>
2026-09-01 18:37:29 -0700

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,5 +2,5 @@ import { Redis } from '@upstash/redis';
 
 export const redis = new Redis({
   url: 'https://witty-anemone-180111.upstash.io',
-  token: 'gQAAAAAAAr-PAAIgcDJwj6riUsFj-FriP9kRfeWJjta8UJ-Fa3KClaxZOifBBQ',
+  token: 'gQAAAAAAAr-PAAIgcDIwy2Y2OTg2YjY3OTY0YzQwOGQwM2M4ZDcxN2E2MDQ2Ng',
 });
